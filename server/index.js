10655ea2793bb0a932cfa073288070b34e44fb4a
Onat <ciftcileronat@gmail.com>
2023-01-05 14:06:18 +0000

    user routes

--- server/index.js
+++ server/index.js
@@ -9,6 +9,12 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+
+import { register } from "./controllers/auth.js";
+import { verifyToken } from "./middleware/auth.js";
+
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -34,6 +40,13 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
