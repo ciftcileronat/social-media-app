ff99dfbf87cff6ed9795857e40c2b82b87d18b8c
Onat <ciftcileronat@gmail.com>
2023-01-06 13:24:03 +0000

    toolkit installation

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
-import HomePage from "scenes/homePage";
-import LoginPage from "scenes/loginPage";
-import ProfilePage from "scenes/profilePage";
+import HomePage from "./scenes/homePage";
+import LoginPage from "./scenes/loginPage";
+import ProfilePage from "./scenes/profilePage";
 
 function App() {
   return (
