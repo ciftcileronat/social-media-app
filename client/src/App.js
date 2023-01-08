06df8ada3cce797fdd7c35400e523f689833fad5
Onat <ciftcileronat@gmail.com>
2023-01-08 11:46:58 +0000

    auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,18 @@
-import React, { useMemo } from "react";
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
+import { useMemo } from "react";
 import { useSelector } from "react-redux";
-
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-
-import HomePage from "./scenes/homePage";
-import LoginPage from "./scenes/loginPage";
-import ProfilePage from "./scenes/profilePage";
 
 function App() {
-  /*
-  const isAuth = Boolean(useSelector((state) => state.token));
-  */
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
+
   return (
     <div className="app">
       <BrowserRouter>
@@ -23,8 +20,14 @@ function App() {
           <CssBaseline />
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/profile/:userId" element={<ProfilePage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
